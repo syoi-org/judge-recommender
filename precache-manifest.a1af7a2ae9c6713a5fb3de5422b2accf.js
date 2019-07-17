dbe82a6048656b6fcfe7527974111fc90b0e9490
jamiechoi <jamie.choi.mail.2002@gmail.com>
2019-07-17 12:52:24 +0800

    Updates

---
+++ precache-manifest.a1af7a2ae9c6713a5fb3de5422b2accf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9ffe21a80994f7ab05d42ba2df6c471",
+    "url": "/syoi/index.html"
+  },
+  {
+    "revision": "8688a76ac95906ff4abe",
+    "url": "/syoi/static/js/2.de184389.chunk.js"
+  },
+  {
+    "revision": "b76de7c3de63a01c39cb",
+    "url": "/syoi/static/js/main.b2d14608.chunk.js"
+  },
+  {
+    "revision": "837c64585b0730e82f64",
+    "url": "/syoi/static/js/runtime~main.772d3bcb.js"
+  },
+  {
+    "revision": "2cd414b9de8080ce7d04ebbaa1ad29a9",
+    "url": "/syoi/static/media/logo.2cd414b9.png"
+  }
+]);
