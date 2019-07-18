149cf6fb77d2996ba5aa4c841ab5756c0d8de2db
jamiechoi <jamie.choi.mail.2002@gmail.com>
2019-07-18 23:27:38 +0800

    Add several icons support

--- src/JudgeIcon.js
+++ src/JudgeIcon.js
@@ -5,21 +5,35 @@ import defaultIcon from './icons/default.ico';
 import hkojIcon from './icons/hkoj.png';
 import cfIcon from './icons/cf.ico';
 import aojIcon from './icons/aoj.ico'
-import csacademyIcon from './icons/csacademy.png';
+import csaIcon from './icons/csa.png';
 import atcoderIcon from './icons/atcoder.png';
+import fbIcon from './icons/fb.png';
+import hackerrankIcon from './icons/hackerrank.png';
+import luoguIcon from './icons/luogu.ico';
+import spojIcon from './icons/spoj.png';
+import syssIcon from './icons/syss.png';
+import kattisIcon from './icons/kattis.png';
 
 function JudgeIcon({ judge }) {
-  const icon = 
-    judge == 'HKOJ' ? hkojIcon :
-    judge == 'CF' ? cfIcon :
-    judge == 'AOJ' ? aojIcon :
-    judge == 'CSAcademy' ? csacademyIcon :
-    judge == 'AtCoder' ? atcoderIcon :
-    defaultIcon;
+  const icons = {
+    'HKOJ': hkojIcon,
+    'CF': cfIcon,
+    'AOJ': aojIcon,
+    'CSA': csaIcon,
+    'AtCoder': atcoderIcon,
+    'FBHC': fbIcon,
+    'HackerRank': hackerrankIcon,
+    'Luogu': luoguIcon,
+    'SPOJ': spojIcon,
+    'SYSS': syssIcon,
+    'Kattis': kattisIcon,
+  };
 
-    return (
-      <Image inline src={icon} style={{height: "1em"}} />
-    );
+  const icon = judge in icons ? icons[judge] : defaultIcon;
+
+  return (
+    <Image inline src={icon} style={{ height: "1em" }} />
+  );
 }
 
 export default JudgeIcon;
